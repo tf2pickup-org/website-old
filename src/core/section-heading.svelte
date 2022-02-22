e856a07d00994ec759f1ebdb1a767d51c0c1804f
Mateusz Delegacz <mat.delegacz@gmail.com>
2022-02-22 23:10:33 +0000

    section-heading component added

---
+++ src/core/section-heading.svelte
@@ -0,0 +1,8 @@
+<script>
+   //Could be done in an abstract way, But would require change in approach to the background text where we use border
+   //instead of image assets
+</script>
+
+<style>
+
+</style>
